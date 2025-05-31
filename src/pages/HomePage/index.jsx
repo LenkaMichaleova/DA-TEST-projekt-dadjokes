85840a844a597897b72158e00676728520e7282a
Lenka Michaleová <michaleova.lenka@gmail.com>
2025-05-31 13:20:49 +0200

    vyřešení druhé části - úkol č.16 - přidán vlastní API endpoint ve složce API a vypsání všech vtipů do stránky

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,40 +1,31 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './style.css';
+import { Joke } from '../../components/Joke';
 
 export const HomePage = () => {
-  const [upLikes, setUpLikes] = useState(0)
-  const [downLikes, setDownLikes] = useState(0)
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+        const response = await fetch("http://localhost:4000/api/users")
+        const json = await response.json()
+        setUsers(json.data)
+    }
+    fetchUsers()
+  }, [])
 
   return (
-    <div className="container">
-      <div className="joke">
-        <div className="joke__body">
-          <div className="joke__user">
-            <img className="user-avatar" src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png" />
-            <p className="user-name">Neroxx</p>
-          </div>
-
-          <p className="joke__text">
-            The secret service isn't allowed to yell "Get down!" anymore when
-            the president is about to be attacked. Now they have to yell
-            "Donald, duck!"
-          </p>
-        </div>
-        <div className="joke__likes">
-          <button 
-            id="btn-up" 
-            className="btn-like btn-like--up"
-            onClick={() => setUpLikes(old => old + 1)}>
-          </button>
-          <span id="likes-up" className="likes-count likes-count--up">{upLikes}</span>
-          <button 
-            id="btn-down" 
-            className="btn-like btn-like--down"
-            onClick={() => setDownLikes(old => old + 1)}>
-          </button>
-          <span id="likes-down" className="likes-count likes-count--down">{downLikes}</span>
-        </div>
-      </div>
-    </div>
+    <>
+      {users.map(user => (
+        <Joke
+          key={user.id} 
+          userAvatar={user.avatar}
+          userName={user.name}
+          text={user.text}
+          likes={user.likes}
+          dislikes={user.dislikes}
+        />
+      ))}
+    </>
   );
 };
